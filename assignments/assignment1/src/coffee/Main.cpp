b301589951b41a00da039d340a59f1fd3a304355
Jacques Nel <jmnel92@gmail.com>
2017-10-01 20:14:52 -0400

    Removed junk from build.

---
+++ assignments/assignment1/src/coffee/Main.cpp
@@ -0,0 +1,17 @@
+// -- Calculations for assignment 1 --
+// -- Cooling Coffee -----------------
+// -- Math 3090 ----------------------
+
+//#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+auto uConst = [](double) -> double { return 0.0; };
+
+int main(int argc, char* argv[]) {}
